77834caa096f498b36bdb168def4c0bd1eec81c7
Pedro Henrique <161644051+Pedro0Oliveira@users.noreply.github.com>
2024-03-08 16:55:06 -0300

    Delete 01_hello_world.py

--- 01_hello_world.py
+++
@@ -1,8 +0,0 @@
-"""01_hello_world.ipynb
-
-Original file is located at
-    https://colab.research.google.com/drive/1XO4Y23BYRNe6jtmYNL6IB-kXVWKnM-gz
-
-Meu primeiro programa
-
-print("Hello world")
